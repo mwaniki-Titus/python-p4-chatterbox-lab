5148aa65b29c1786811f09ae8a195a68110b182b
Titus Munyoki <titus.munyoki@student.moringaschool.com>
2023-09-28 22:18:37 +0300

    completed assingment

--- server/app.py
+++ server/app.py
@@ -14,13 +14,95 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
+@app.route('/messages',methods=['GET','POST'])
 def messages():
-    return ''
+    if request.method == 'GET':
+        messages=[]
+        for message in Message.query.order_by(Message.created_at).all():
+            message_dict = message.to_dict()
+            messages.append(message_dict)
+        response=make_response(
+            jsonify(messages),
+            200
+        )   
 
-@app.route('/messages/<int:id>')
+        return response
+    elif request.method == 'POST':
+        data = request.get_json()
+        body = data.get('body')
+        username = data.get('username')
+        new_message=Message(
+            body=body,
+            username=username,
+        )
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict=new_message.to_dict()
+
+        response=make_response(
+            jsonify(message_dict),
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>',methods=['GET','PATCH','DELETE'])
 def messages_by_id(id):
-    return ''
+    message = Message.query.filter_by(id=id).first()
+
+    if message==None:
+        response_body = {
+            "message": "This record does not exist in our database. Please try again."
+        }
+        response = make_response(jsonify(response_body), 404)
+
+        return response
+    else:
+        if request.method == 'GET':
+            message_dict = message.to_dict()
+
+            response = make_response(
+                jsonify(message_dict),
+                200
+            )
+
+            return response
+
+        elif request.method == 'PATCH':
+            data = request.get_json()
+            if 'body' in data:
+                message.body = data['body']
+            if 'username' in data:
+               message.username = data['username']
+
+            db.session.commit()
+
+            message_dict = message.to_dict()
+
+            response = make_response(
+                jsonify(message_dict),
+                200
+            )
+
+            return response
+
+        elif request.method == 'DELETE':
+            db.session.delete(message)
+            db.session.commit()
+
+            response_body = {
+                "delete_successful": True,
+                "message": "Message deleted."    
+            }
+
+            response = make_response(
+                jsonify(response_body),
+                200
+            )
+
+            return response
+
 
 if __name__ == '__main__':
     app.run(port=5555)
